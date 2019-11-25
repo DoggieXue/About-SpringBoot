0319dcd3cb4fa87ad7313b5243f7bea35ee254ef
xuexiao <xuexiao@belink.com>
2019-11-25 09:21:43 +0800

    添加文件上传所需依赖

---
+++ src/main/java/com/cloud/xue/demo/controller/UploadFileController.java
@@ -0,0 +1,11 @@
+package com.cloud.xue.demo.controller;
+
+/**
+ * @Program demo
+ * @Title: UploadFileController
+ * @Description:
+ * @Author: XueXiao
+ * @Create: 2019-11-21 09:48:28
+ */
+public class UploadFileController {
+}
---
+++ src/main/java/com/cloud/xue/demo/utils/UploadUtil.java
@@ -0,0 +1,11 @@
+package com.cloud.xue.demo.utils;
+
+/**
+ * @Program demo
+ * @Title: UploadUtil
+ * @Description:
+ * @Author: XueXiao
+ * @Create: 2019-11-21 10:13:41
+ */
+public class UploadUtil {
+}
