08319ccc026ed27982cabe703bac78bc2dbabea4
xuexiao <xuexiao@belink.com>
2019-11-26 09:31:51 +0800

    调整单元测试案例

--- src/test/java/com/cloud/xue/demo/dao/AreaDaoTest.java
+++ src/test/java/com/cloud/xue/demo/dao/AreaDaoTest.java
@@ -24,7 +24,7 @@ public class AreaDaoTest {
     @Test
     public void queryArea() {
         List<Area> areaList = areaDao.queryArea();
-        assertEquals(2, areaList.size());
+        assertEquals(5, areaList.size());
     }
 
     @Test
