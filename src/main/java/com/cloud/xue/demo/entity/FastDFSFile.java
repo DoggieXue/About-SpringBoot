99d220ddce0b47deee1557e8a1a7c90c71844646
xuexiao <xuexiao@belink.com>
2019-11-27 14:14:03 +0800

    引入FastDFS依赖

---
+++ src/main/java/com/cloud/xue/demo/entity/FastDFSFile.java
@@ -0,0 +1,11 @@
+package com.cloud.xue.demo.entity;
+
+/**
+ * @Program demo
+ * @Title: FastDFSFile
+ * @Description:
+ * @Author: XueXiao
+ * @Create: 2019-11-26 22:10:18
+ */
+public class FastDFSFile {
+}
---
+++ src/main/java/com/cloud/xue/demo/utils/FastDFSClient.java
@@ -0,0 +1,11 @@
+package com.cloud.xue.demo.utils;
+
+/**
+ * @Program demo
+ * @Title: FastDFSClient
+ * @Description:
+ * @Author: XueXiao
+ * @Create: 2019-11-26 22:13:07
+ */
+public class FastDFSClient {
+}
